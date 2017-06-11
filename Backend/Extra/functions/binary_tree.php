d5cdf2fc142519fac7aac0e4880d7d4fd0d77ccc
amatiyko <andrew.matiyko@gmail.com>
2017-06-11 16:19:34 +0300

    .

--- Backend/Extra/functions/binary_tree.php
+++ Backend/Extra/functions/binary_tree.php
@@ -1,24 +1,43 @@
 <?php
 for ($i=0; $i<=20; $i++) {
-    $arr[$i] = rand(0, 10000);
+    $arr[$i] = rand(0, 1000);
     echo $arr[$i] . ' ';
 }
 
-function binary_tree_print($a)
-{
-    $raw_stop = 1;
-    $marker =1;
-    echo '<div style="text-align:center; font-size: 20px; line-height: 40px;">';
-    foreach ($a as $value) {
-        echo $value  . '<span style="padding-left: 30px;"></span>';
-        $marker++;
-        if ($marker > $raw_stop) {
-          echo '<br/>';
-          $marker = 1;
-          $raw_stop++;
-        }
-    }
-    echo '</div>';
-}
-binary_tree_print($arr);
+//function binary_tree_print($a)
+//{
+//    $raw_stop = 1;
+//    $marker =1;
+//    echo '<div style="text-align:center; font-size: 20px; line-height: 40px;">';
+//    foreach ($a as $value) {
+//        echo $value  . '<span style="padding-left: 30px;"></span>';
+//        $marker++;
+//        if ($marker > $raw_stop) {
+//          echo '<br/>';
+//          $marker = 1;
+//          $raw_stop++;
+//        }
+//    }
+//    echo '</div>';
+//}
+//binary_tree_print($arr);
 
+function binary_tree_sort($a)
+{
+    $binary_tree = [ $parent => $a[0];]
+
+//    $parent = $a[0];
+//    $left_child = $a[1];
+//    $right_child = $a[2];
+//    $binary_tree = [
+//        $parent => [
+//            $left_child =>[],
+//            $right_child => []
+//        ]
+//    ];
+//    echo '<pre>';
+//    print_r($binary_tree);
+//    echo '</pre>';
+
+}
+binary_tree_sort($arr);
